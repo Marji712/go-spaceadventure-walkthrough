838bd8e75bcc1b9b2327fcf782a2b1052b5af26d
Marji Symonds <symondsm@oregonstate.edu>
2019-11-19 15:18:54 -0800

    Add mockPlanets function to create planets

--- cmd/spaceadventure/main.go
+++ cmd/spaceadventure/main.go
@@ -3,7 +3,23 @@ package main
 import "go-spaceadventure-walkthrough/internal/spaceadventure"
 
 func main() {
-	var ps = spaceadventure.PlanetarySystem{Name:"Solar System", Planets: []spaceadventure.Planet{
-		spaceadventure.Planet{"Tatooine", "Desert planet"}},}
-	spaceadventure.Start(ps)
+	spaceadventure.Start(
+		spaceadventure.PlanetarySystem{Name:"Solar System", Planets: mockPlanets()},
+	)
+}
+
+func mockPlanets() []spaceadventure.Planet {
+	return []spaceadventure.Planet{
+		spaceadventure.Planet{"Tatooine", "Desert planet"},
+		spaceadventure.Planet{"Dagobah", "Swamp planet"},
+		spaceadventure.Planet{"Hoth", "Icy planet"},
+		spaceadventure.Planet{"Jaku", "Party planet"},
+		spaceadventure.Planet{"Mustafar", "Fiery planet"},
+		spaceadventure.Planet{"Coruscant", "Urban planet"},
+		spaceadventure.Planet{"Bespin", "Cloudy planet"},
+		spaceadventure.Planet{"Naboo", "Temple planet"},
+		spaceadventure.Planet{"Kashyyyk", "Swampy planet"},
+		spaceadventure.Planet{"Endor", "Ewok planet"},
+		spaceadventure.Planet{"Alderaan", "Gone planet"},
+	}
 }
